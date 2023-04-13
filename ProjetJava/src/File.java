800c8b4bc34d71892419d479323eddca54d5addf
Naki00001 <122391815+Naki00001@users.noreply.github.com>
2023-04-13 14:31:25 +0200

    Update File.java

--- ProjetJava/src/File.java
+++ ProjetJava/src/File.java
@@ -1,5 +1,12 @@
 package JavaProject;
-
+import java.io.File;
+import java.nio.file.StandardCopyOption;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 public class File {
+  private ArrayList<String> targetFileName =New ArrayList<String>(); 
+  private ArrayList<String> srcFileName =New ArrayList<String>();
 
 }
