1963491ecaf373e911b45b5dc6666fef70ab0b29
Naki00001 <122391815+Naki00001@users.noreply.github.com>
2023-04-13 19:39:26 +0200

    Add files via upload

---
+++ Javadirectory.java
@@ -0,0 +1,21 @@
+package javadirectory;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Javadirectory{
+private String pathS;
+private String PathD;
+Javadirectory(String pathS,String pathD){
+    this.pathS=pathS;
+    this.PathD=pathD;
+}
+public String readPath(char directory){
+	if(directory == 's') return PathD;
+	if(directory == 't') return PathD;
+	return null;
+}
+public static void main(String[] args) {
+	
+}
+}
