440823d3ca2625699a9b151e62af2609341d0e32
HOUMADI.COMBO <122391815+Naki00001@users.noreply.github.com>
2023-04-18 13:57:06 +0200

    Update Javadirectory.java

--- ProjetJava/src/javadirectory/Javadirectory.java
+++ ProjetJava/src/javadirectory/Javadirectory.java
@@ -4,53 +4,57 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Javadirectory{
-private static int security = 0 ;
-private static int reseaux=0;
-//0 pour synchronisation local 1 pour synchrosisation réseaux
-private String pathS;//0
-private String pathD;//1
-Javadirectory(String pathS,String pathD){
-    this.pathS=pathS;
-    this.pathD=pathD;
-}
-public String readPath(int directory){
-	String S = this.pathS;
-	String D = this.pathD;
-	switch(directory) {
-	case(0):
-	    return S;
-	case(1):
-	    return D;
-	default:
-		System.out.println("ECHEC");
-		return null;
+	private static int security = 0 ;
+	private static int reseaux;
+	//0 pour synchronisation local 1 pour synchrosisation réseaux
+	private String pathS;//0
+	private String pathD;//1
+	Javadirectory(String pathS,String pathD){
+		this.pathS=pathS;
+		this.pathD=pathD;
 	}
-}
-public static void main(String[] args) {
-	Scanner saisieUtilisateur = new Scanner(System.in);
-	reseaux = saisieUtilisateur.nextInt();
-	switch(reseaux){
-		case 0 :
-			while (security == 0) {
-				Running synch = new Running();
-				synch.start();
-				try {
-					//int une_semaine = 1000*60*60*24*7;
-					int dix_secondes =1000*10;
-					Thread.sleep(dix_secondes);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+	public String readPath(int directory){
+		String S = this.pathS;
+		String D = this.pathD;
+		switch(directory) {
+			case(0):
+				return S;
+			case(1):
+				return D;
+			default:
+				System.out.println("ECHEC");
+				return null;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner saisieUtilisateur = new Scanner(System.in);
+		System.out.println("Voulez vous synchroniser des dossiers sur deux ordinateurs différents de votre réseau local ? (taper 1 pour oui 0 pour non) :");
+		reseaux = saisieUtilisateur.nextInt();
+		if(reseaux == 0)System.out.println("Vous avez choisis de synchroniser deux dossiers de votre disc local. ");
+		if(reseaux == 1)System.out.println("Vous avez choisis de synchroniser deux dossiers sur des discs distants.  ");
+		switch(reseaux){
+			case 0 :
+				while (security == 0) {
+					Running synch = new Running();
+					synch.start();
+					try {
+						//int une_semaine = 1000*60*60*24*7;
+						int dix_secondes =1000*10;
+						Thread.sleep(dix_secondes);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("Voulez vous continuer la synchronisation de vos dossiers? (taper 0 pour oui 1 pour non) :");
+					security = saisieUtilisateur.nextInt();
 				}
-				System.out.println("Veuillez saisir un entier :");
-				security = saisieUtilisateur.nextInt();
-			}
-			System.out.println("Fin de la synchronisation de vos dossiers ");
-			break;
-		case 1 :
-			break;
-		default:
-			System.out.println("Error!!!!!");
-			break;
+				System.out.println("Fin de la synchronisation de vos dossiers ");
+				break;
+			case 1 :
+				System.out.println("Error!!!!!");
+				break;
+			default:
+				System.out.println("Error!!!!!");
+				break;
+		}
 	}
-}
 }
